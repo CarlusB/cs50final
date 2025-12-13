e1c4ecf64185ccd27e351606bc593722d5df8511
Carlus Brown <carlusbrown@gmail.com>
2025-12-13 16:50:28 -0600

    created layout for card search and card details

--- app.py
+++ app.py
@@ -9,3 +9,11 @@ def index():
 @app.route("/vid")
 def vid():
     return render_template("vid.html")
+
+@app.route("/card_search")
+def card_search():
+    return render_template("card_search.html")
+
+@app.route("/card")
+def card():
+    return render_template("card.html")
