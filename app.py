35aff0310ca7d069948771526468497f92a8c69e
Carlus Brown <carlusbrown@gmail.com>
2025-12-31 13:58:24 -0600

    created error page

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request
+from pokemontcgsdk import Card # type: ignore
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+
+pokemontcg_io_api_key = os.environ.get("POKEMONTCG_IO_API_KEY")
 
 @app.route("/")
 def index():
@@ -10,13 +17,22 @@ def index():
 def vid():
     return render_template("vid.html")
 
-@app.route("/card_search")
+@app.route('/card_search', methods=['POST', 'GET'])
 def card_search():
-    return render_template("card_search.html")
+    cards = None
+    if request.method == 'POST':
+        try:
+            cards = Card.where(q='set.name:generations supertype:pokemon')
+            return render_template("card_search.html", cards=cards)
+        except KeyError:
+            return render_template("error.html", 400)
+    else:
+        return render_template("card_search.html")
 
 @app.route("/card")
 def card():
-    return render_template("card.html")
+    card = Card.find('xy1-1')
+    return render_template("card.html", card=card)
 
 @app.route("/card_listing")
 def card_listing():
