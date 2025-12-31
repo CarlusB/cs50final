55149311fbc4b9763de3d6334dcb41927eecd7a3
Carlus Brown <carlusbrown@gmail.com>
2025-12-31 17:21:37 -0600

    added mysql transactions

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
-import os
 from flask import Flask, render_template, request
-from pokemontcgsdk import Card # type: ignore
-from dotenv import load_dotenv
-
-load_dotenv()
+import config
+import json
+import MySQLdb
 
 app = Flask(__name__)
 
-pokemontcg_io_api_key = os.environ.get("POKEMONTCG_IO_API_KEY")
+print(config.POKEMONTCG_IO_API_KEY)
 
 @app.route("/")
 def index():
@@ -19,19 +17,26 @@ def vid():
 
 @app.route('/card_search', methods=['POST', 'GET'])
 def card_search():
-    cards = None
-    if request.method == 'POST':
-        try:
-            cards = Card.where(q='set.name:generations supertype:pokemon')
-            return render_template("card_search.html", cards=cards)
-        except KeyError:
-            return render_template("error.html", 400)
-    else:
-        return render_template("card_search.html")
+    con = MySQLdb.connect(
+        host=config.HOST,
+        user=config.USER,
+        password=config.PASSWORD,
+        database=config.DATABASE
+    )
+    cur = con.cursor()
+
+    cur.execute("SELECT details FROM cards")
+    cards = json.loads(cur.fetchall())
+    images = []
+    for card in cards:
+        images.append(card["images"]["small"])
+
+    cur.close()
+    con.close()
+    return render_template("card_search.html", images = images)
 
 @app.route("/card")
 def card():
-    card = Card.find('xy1-1')
     return render_template("card.html", card=card)
 
 @app.route("/card_listing")
